20998d24f525cd9120c774a2587ebd5c387ef4b7
FatchiaFahri <fatchiafahri@gmail.com>
2022-12-14 20:45:30 +0700

    push ke github

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "FATCHIA FAHRI" // TODO: replace this
+const studentId = "FE2605697" // TODO: replace this
+
+export { url, studentName, studentId }
